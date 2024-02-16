2c8fdc9936314f213b2ee530b4b5a826031f4b72
Juan P. Romano <programming@jpromano.net>
2024-02-16 11:39:27 -0300

    Add files via upload

---
+++ InstallXS/htdocs/idiomas/en/principal.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# InstallXS v0.1
+#
+# Autor: Pablo Manuel Rizzo <info@pablorizzo.com>
+#
+# Copyright (C) 2007 The UTUTO Project
+# Distributed under the terms of the GNU General Public License v3 or newer
+#
+# $Header: $
+
+LANG_TITULOENCABEZADO="<b>InstallXS</b> generic GNU installer"
+LANG_NEXT="Next >>"
+LANG_REFRESH="Refresh (CTRL + R)"
+LANG_REBOOT="Reboot"
+LANG_DEFAULT_TITLE="UTUTO XS installation"
+LANG_INSTALATION_TYPE_AUTO="Automatica installation"
+LANG_INSTALATION_TYPE_MANUAL="Manual installation"
+
+LANG_ERR_NO_MODULE="Error: no initial module configured"
+
+NOMBRE_STAGE1="$NOMBRE_STAGE1"
+NOMBRE_STAGE2="$NOMBRE_STAGE2"
+NOMBRE_STAGE3="$NOMBRE_STAGE3"
+
+LANG_FINAL_THANKS="<b>El equipo de UTUTO XS:</b>
+<pre>Daniel Olivera - Diego Saravia
+Martin Gómez Gimenez - Diego Calbo - 
+Gaby Campo - Federico Casares - Pablo
+Rizzo - Cristobal López - Martin Lima -
+Verónica Xhardez - Anielkis Herrera - Angel
+Goni - Angel Matias Orozco - Quiliro Ordóñez
+Grupo EATCO (UCO) - Fundacion I2BC.
+</pre>
+"
+
+LANG_FINAL_REBOOT="<small>Press <b>$LANG_REBOOT</b> to start $NOMBRE_STAGE1...</small>"
+
---
+++ InstallXS/htdocs/idiomas/es/principal.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# InstallXS v0.1
+#
+# Autor: Pablo Manuel Rizzo <info@pablorizzo.com>
+#
+# Copyright (C) 2007 The UTUTO Project
+# Distributed under the terms of the GNU General Public License v3 or newer
+#
+# $Header: $
+
+LANG_TITULOENCABEZADO="<b>InstallXS</b> Instalador generico GNU"
+LANG_NEXT="Avanzar >>"
+LANG_REFRESH="Refrescar (CTRL + R)"
+LANG_REBOOT="Reiniciar"
+LANG_DEFAULT_TITLE="Instalacion de UTUTO XS"
+LANG_INSTALATION_TYPE_AUTO="Instalación automatica"
+LANG_INSTALATION_TYPE_MANUAL="Instalación manual"
+
+LANG_ERR_NO_MODULE="Error: no existe un módulo inicial"
+
+NOMBRE_STAGE1="$NOMBRE_STAGE1"
+NOMBRE_STAGE2="$NOMBRE_STAGE2"
+NOMBRE_STAGE3="$NOMBRE_STAGE3"
+
+
+LANG_FINAL_THANKS="<b>El equipo de UTUTO XS:</b>
+<pre>Daniel Olivera - Diego Saravia
+Martin Gómez Gimenez - Diego Calbo - 
+Gaby Campo - Federico Casares - Pablo
+Rizzo - Cristobal López - Martin Lima -
+Verónica Xhardez - Anielkis Herrera - Angel
+Goni - Angel Matias Orozco - Quiliro Ordóñez
+Grupo EATCO (UCO) - Fundacion I2BC.
+</pre>
+"
+
+LANG_FINAL_REBOOT="<small>Presione <b>$LANG_REBOOT</b> para iniciar $NOMBRE_STAGE1...</small>"
+
